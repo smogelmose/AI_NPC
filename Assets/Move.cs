6875c1f3154e13a981c6f6e67a2c1e8159809a42
Steffen Møgelmose <steffen@mogelmose.org>
2025-02-25 20:14:12 +0100

    Initial commit

---
+++ Assets/Move.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 direction;
+    public float speed = 5f;
+
+    void Start() {
+
+        //direction = goal.transform.position - transform.position;
+       
+        //transform.Translate(direction);
+      
+    }
+
+    private void LateUpdate() 
+    {
+         direction = goal.transform.position - transform.position;
+         transform.LookAt(goal.transform.position);
+         if (direction.magnitude > 2)
+         {
+            Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+            transform.position = transform.position + velocity;
+         }
+
+    }
+}
